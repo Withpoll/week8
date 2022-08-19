5c79d29adc032e85b15c507c6239681ef5f9b20f
Withpoll <109366621+Withpoll@users.noreply.github.com>
2022-08-20 00:33:32 +0800

    init

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+module.exports = {
+  solidity: "0.8.4",
+
+  networks: {
+    
+    "optimism": {
+       url: process.env.URL,
+       accounts: [ process.env.MNEMONIC ]
+    }
+  }
+};
